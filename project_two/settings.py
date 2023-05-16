595e24a07778f4a72d5d22478e6a55b99765053b
root <templeclancy333@icloud.com>
2023-05-16 22:06:12 +0000

    fixing things

--- project_two/settings.py
+++ project_two/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--np)7op(#yr+(1^3qo2nm!+4o50(*9(-*_jc8*0pdj+d#s1uhn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
